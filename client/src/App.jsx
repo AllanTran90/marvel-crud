d21bbc1128635a43c9afbea679f20a00f06c7d39
AllanTran90 <allan_sweden90@hotmail.com>
2026-08-23 13:25:14 +0200

    Add useEffect to fetch movies from API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   
+useEffect (() =>{
+  fetch('http://localhost:3001/api/movies')
+    .then((res) => res.json())
+    .then((data) => setMovies(data))
+    .catch((err) => console.error('Something went wrong in fetch:', err));
+}, []);
+
   return (
     <div>
       <h1>
